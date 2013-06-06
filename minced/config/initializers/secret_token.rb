ee3253238a5230ac0ecabd62d30a3a9bed627915
A K McCrate <akmccrate@gmail.com>
2013-06-05 21:41:04 -0700

    Create new rails app, Minced

---
+++ minced/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minced::Application.config.secret_token = '25520aa7f409845c3c1d78dd10a77945ae9f9dfbc5417b27d148b33350dc22b3781eae0924f1afd6891bc0485c639e561449ec249e994e40bf6dd83b2c6ad006'
